5e54489b1251bbaceb2fc8619110e0e86c07dbd5
bingli7 <your_email>
2018-06-20 16:30:24 +0800

    New updates

--- NewInstances.py
+++ NewInstances.py
@@ -14,7 +14,8 @@ from jdcloud_sdk.services.vm.apis.CreateInstancesRequest import *
 
 class NewInstances(object):
 
-    def __init__(self, regionId, az, instanceType, imageId, name, password):
+    def __init__(self, access_key, secret_key, regionId, az, instanceType, imageId,\
+                 name, password, maxCount=1, chargeSpec=None, description=None):
         self.regionId = regionId
         self.az = az
         # E.G. 'g.s1.micro'
@@ -23,15 +24,16 @@ class NewInstances(object):
         # instance name prefix
         self.name = name
         self.password = password
+        self.maxCount = maxCount
+        # Default None: postpaid_by_duration:按配置后付费
+        self.chargeSpec = chargeSpec
+        self.description = description
         # InstanceDiskAttachmentSpec[]
         self.dataDiskSpec = []
-        print "Initiating finish..."
 
-    def setUpClient(self, access_key, secret_key):
-        # your access key and private secret key
-        my_access_key = access_key
-        my_secret_key = secret_key
-        self.credential = Credential(my_access_key, my_secret_key)
+        self.elasticIpSpec = None
+
+        self.credential = Credential(access_key, secret_key)
         self.client = VmClient(self.credential)
         print 'Client created...'
 
@@ -63,26 +65,15 @@ class NewInstances(object):
         print 'DataDiskSpec created...'
         print 'DataDisk: ' + new_instanceDiskAttachmentSpec.diskCategory
 
-    def setElasticIp(self, bandwidthMbps, provider, chargeSpec=None):
+    def setElasticIp(self, bandwidthMbps, provider):
         #  bandwidthMbps(Integer), provider: 'bgp' or 'no_bgp'
-        self.elasticIpSpec = ElasticIpSpec(bandwidthMbps, provider, chargeSpec)
+        self.elasticIpSpec = ElasticIpSpec(bandwidthMbps, provider, self.chargeSpec)
         print 'ElasticIPSpec created...'
         print 'BandWidth: ', self.elasticIpSpec.bandwidthMbps, '  provider: ', self.elasticIpSpec.provider
 
-    def setDescription(self, description):
-        # Description for the instances
-        self.description = description
-        print 'Description created...'
-        print 'Description: ' + self.description
-
-    def setMaxCount(self, maxCount):
-        # Numbers of instances
-        self.maxCount = maxCount
-        print 'MaxCount created...'
-        print 'InstanceCount: ', self.maxCount
-
     def createInstances(self):
         # Create instances and get the response
+        # Return list: str[]
         print 'Creating instances...'
         self.instanceSpec = InstanceSpec(az=self.az, instanceType=self.instanceType, imageId=self.imageId,\
                                          name=self.name, keyNames=None, primaryNetworkInterface=self.networkSpec,\
@@ -105,47 +96,52 @@ class NewInstances(object):
         if resp.error is not None:
             print resp.error.code, resp.error.message
 
-        return resp
+        my_resp = []
+        #from unicode to utf8
+        for i in resp.result['instanceIds']:
+            my_resp.append(i.encode("utf-8"))
+
+        return my_resp
 
 if __name__ == '__main__':
     # parameters:
     regionId = 'cn-north-1'
     az = 'cn-north-1a'
     instanceType = 'g.s1.micro'
-    instancename = 'pocenv-libing-apitest-0617'
+    instancename = 'pocenv-libing-apitest-0620-'
     password = 'HelloWorld@618'
     imageId = '5de362a8-b5c7-4bc6-8b80-a7fab91475db'
-
-    myinstances = NewInstances(regionId=regionId, az=az, instanceType=instanceType,\
-                               imageId=imageId, name=instancename, password=password)
-
-    # Set up VmClient
     access_key = 'ak'
     secret_key = 'sk'
-    myinstances.setUpClient(access_key, secret_key)
+    maxCount = 2
+    description = "libing testing 20180620"
+    myinstances = NewInstances(access_key=access_key, secret_key=secret_key,regionId=regionId,\
+                               az=az, instanceType=instanceType, imageId=imageId,\
+                               name=instancename, password=password, maxCount=maxCount,\
+                               description=description)
+
     # Set up network
     subnetId = 'subnet-pryeobh0fd'
     myinstances.setNetworkSpec(subnetId)
-    # Set up charge
-    chargeMode = 'postpaid_by_duration'
-    myinstances.setChargeSpec(chargeMode)
     # Set up system disk
     myinstances.setSystemDisk()
-    # Set up data disk
-    dataDiskName = 'libing-test-'
+    # Set up a data disk
+    dataDiskName = 'libing-test01-'
+    dataDiskSize = 20
+    dataDiskType = 'ssd'
+    myinstances.setDataDisk(dataDiskName, dataDiskSize, dataDiskType)
+    # Set up a second data disk
+    dataDiskName = 'libing-test02-'
     dataDiskSize = 30
     dataDiskType = 'premium-hdd'
     myinstances.setDataDisk(dataDiskName, dataDiskSize, dataDiskType)
     # Set up elastic IP
-    elasticIpBW = 3
+    elasticIpBW = 1
     elasticIpprovider = 'bgp'
     myinstances.setElasticIp(elasticIpBW, elasticIpprovider)
-    # Set up description for all instances
-    instanceDescription = 'libing testing API 0617'
-    myinstances.setDescription(instanceDescription)
-    # Set up numbers of instances
-    instancesCount = 3
-    myinstances.setMaxCount(instancesCount)
 
     # Finally create instances
-    myinstances.createInstances()
+    resp = myinstances.createInstances()
+
+    print resp
+    # ['i-cp6gidfe23', 'i-h96pw8ngor']
